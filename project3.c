7896725a79af693e06f0cabf2906d366d7a915dd
Seth Charles Polen <polen@linprog7.cs.fsu.edu>
2020-11-17 16:12:14 -0500

    added basic structure to program

--- project3.c
+++ project3.c
@@ -1,2 +1,23 @@
 // this is our proj3 file
+#include <iostream>
 
+using namespace std;
+
+/*void exit(){
+	//safely exit program
+	//free up resources
+}*/
+
+while(command != "exit"){
+	print("$ ");
+	read_from_stdin(command);
+	if(command == "info"){
+		//do stuff
+	}
+	else if(command == "ls"){
+		//do stuff
+	}
+	else if(){
+
+	}
+}
