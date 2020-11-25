4eec38d82a58edad83d922637fdd63e664de4820
Teresa Marie Weaver <teweaver@linprog3.cs.fsu.edu>
2020-11-25 10:20:43 -0500

    info function kinda done

--- project3.c
+++ project3.c
@@ -55,12 +55,32 @@ void free_tokens(tokenlist *tokens);
 
 int main()
 {
-	f32.a = fopen("fat32.img", "rb"); //idk if rb is right
+	struct fileinfo f32;
+	f32.fileID = open("fat32.img", O_RDWR); 
 	int i; 
-	fileinfo pt[4];
-    
-    fseek(in, 0x1BE, SEEK_SET); // go to partition table start
-    fread(pt, sizeof(fileinfo), 4, in); // read all four entries
+	buffer = malloc(32);
+	i = read(f32.fileID, buffer, 2, 11); //i = number of bytes read 
+	//flip it
+    f32.BPB_BytesPerSector = buffer;
+	
+	i = read(f32.fileID, buffer, 1, 13); //i = number of bytes read 
+	//flip it
+    f32.BPB_SecPerClus = buffer;
+
+    i = read(f32.fileID, buffer, 2, 14); //i = number of bytes read 
+	//flip it
+    f32.BPB_RsvdSecCnt = buffer;
+
+    i = read(f32.fileID, buffer, 4, 32); //i = number of bytes read 
+	//flip it
+    f32.BPB_TotSec32= buffer;
+
+    i = read(f32.fileID, buffer, 4, 44); //i = number of bytes read 
+	//flip it
+    f32.BPB_RootClus = buffer;
+
+   
+
    
 	while (1) {
 		printf("$ ");
